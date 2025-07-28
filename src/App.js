3461922534936c6f1f048ce9180d76ad0b16db62
Camille Legaspi <camillelegaspi@Camilles-MacBook-Pro-13in.local>
2025-07-28 16:42:11 -0400

    added Clarifai

--- src/App.js
+++ src/App.js
@@ -4,16 +4,92 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import 'tachyons';
 import Rank from './components/Rank/Rank';
-import ParticlesBg from 'particles-bg'
+import ParticlesBg from 'particles-bg';
+import Clarifai from 'clarifai';
+
+const app = new Clarifai.App({
+  apiKey: '7ffedc701efc429cb3182c6be0326dc7'
+});
+
+const returnClarifaiRequest=(imageUrl) =>{
+  // Your PAT (Personal Access Token) can be found in the Account's Security section
+  const PAT = 'cc776900c35d4648b41baafd82a263f5';
+  // Specify the correct user_id/app_id pairings
+  // Since you're making inferences outside your app's scope
+  const USER_ID = 'clegaspi';
+  const APP_ID = 'myFirstApp';
+  // Change these to whatever model and image URL you want to use
+  const MODEL_ID = 'face-detection';
+  const IMAGE_URL = imageUrl;
+  const raw = JSON.stringify({
+    "user_app_id": {
+        "user_id": USER_ID,
+        "app_id": APP_ID
+    },
+    "inputs": [
+        {
+            "data": {
+                "image": {
+                    "url": IMAGE_URL
+                    // "base64": IMAGE_BYTES_STRING
+                }
+            }
+        }
+    ]
+});
+
+const requestOptions = {
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'Authorization': 'Key ' + PAT
+    },
+    body: raw
+};
+return requestOptions;
+}
 
 function App() {
+  const onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+  const onBtnClick = () => {
+    console.log('click');
+    fetch("https://api.clarifai.com/v2/models/" + 'face-detection' + "/outputs", returnClarifaiRequest(this.state.input))
+    .then(response => response.json())
+    .then(result => {
+
+        const regions = result.outputs[0].data.regions;
+
+        regions.forEach(region => {
+            // Accessing and rounding the bounding box values
+            const boundingBox = region.region_info.bounding_box;
+            const topRow = boundingBox.top_row.toFixed(3);
+            const leftCol = boundingBox.left_col.toFixed(3);
+            const bottomRow = boundingBox.bottom_row.toFixed(3);
+            const rightCol = boundingBox.right_col.toFixed(3);
+
+            region.data.concepts.forEach(concept => {
+                // Accessing and rounding the concept value
+                const name = concept.name;
+                const value = concept.value.toFixed(4);
+
+                console.log(`${name}: ${value} BBox: ${topRow}, ${leftCol}, ${bottomRow}, ${rightCol}`);
+                
+            });
+        });
+
+    })
+    .catch(error => console.log('error', error));
+    
+  }
   return (
     <div className="App">
       <ParticlesBg type="thick" bg={true} />
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm />
+      <ImageLinkForm onInputChange={onInputChange} onBtnClick={onBtnClick}/>
       {/* <FaceRecognition /> */}
     </div>
   );
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,12 +1,12 @@
-const ImageLinkForm=()=> {
+const ImageLinkForm=({onInputChange, onBtnClick})=> {
     return(
       <div>
-        <p className="f3">
+        <p className="f3 lightest-blue">
             {'This Magic Brain will detect faces in your pictures. Give it a try.'}
         </p>
         <div className="pa4 br3 shadow-3 w-80 center">
-            <input className='f4 pa2 w-70 center' type="text" />
-            <button className='w-30 grow f4 link ph3 pv2 dib white bg-blue'>Detect</button>
+            <input className='f4 pa2 w-70 center' type="text" onChange={onInputChange} />
+            <button className='w-30 grow f4 link ph3 pv2 dib white bg-blue'onClick={onBtnClick}>Detect</button>
         </div>
       </div>  
     )
